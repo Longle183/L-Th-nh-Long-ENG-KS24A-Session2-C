7d25c566cc3c1fe99075e329470b0f68384b76ae
Longle183 <lethanhlong1224@gmail.com>
2024-11-21 12:58:20 +0700

    Add files via upload

---
+++ btvn session2 04.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	
+	int canh = 5;
+	int chuVi = canh * 4;
+	int dienTich = canh * canh;
+	printf("Canh cua hinh vuong la : %d\n",canh);
+	printf("Chu vi cua hinh vuong la : %d\n",chuVi);
+	printf("Dien tich cua hinh vuong la : %d\n",dienTich);
+	
+	return 0;
+}
