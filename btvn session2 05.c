0e8869edc9f29989d1f5faebb292c0523ed3bfa7
Longle183 <lethanhlong1224@gmail.com>
2024-11-21 12:58:33 +0700

    Add files via upload

---
+++ btvn session2 05.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	int chieuDai = 5;
+	int chieuRong = 4;
+	int chuVi = (chieuDai + chieuRong) * 2;
+	int dienTich = chieuDai * chieuRong;
+	printf("Chieu dai cua hinh chu nhat la : %d\n",chieuDai);
+	printf("Chieu rong cua hinh chu nhat la : %d\n",chieuRong);
+	printf("Chu vi cua hinh chu nhat la : %d\n",chuVi);
+	printf("Dien tich cua hinh chu nhat la : %d\n, dienTich",dienTich);
+	
+	return 0;
+}
