efac58bd591f9267d3c17fe5d42d8db4bc6017a3
Longle183 <lethanhlong1224@gmail.com>
2024-11-21 12:57:59 +0700

    Add files via upload

---
+++ btvn session2 03.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	int num1 = 20;
+	int num2 = 10;
+	int tong = num1 + num2;
+	int hieu = num1 - num2;
+	int tich = num1 * num2;
+	int thuong = num1 / num2;
+	printf("Tong cua %d va %d la: %d\n",num1, num2, tong);
+	printf("Hieu cua %d va %d la: %d\n",num1, num2, hieu);
+	printf("Tich cua %d va %d la: %d\n",num1, num2, tich);
+	printf("Thuong cua %d va %d la: %d\n",num1, num2, thuong);
+	
+	return 0;
+}
